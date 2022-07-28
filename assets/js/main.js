9b07fc038a0555d634d30f6c3b18a417e6ee199a
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-07-28 19:25:56 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,16 @@
 		}
 
 })(jQuery);
+
+let card = document.querySelector('.card');
+card.addEventListener('click', click);
+
+
+function click(event) {
+	let elem = event.currentTarget;
+	if (elem.style.transform == "rotateY(180deg)") {
+		elem.style.transform = "rotateY(0deg)";
+	} else {
+		elem.style.transform = "rotateY(180deg)";
+	}
+}
